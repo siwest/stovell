9244ed3f721b74ae39a4f8c4788ec98a567e2a58
Sarah West <sarahwest@Sarahs-MacBook-Pro.local>
2020-10-07 11:47:52 -0700

    Adds helper functions and docstrings

--- stovell/stock_portfolio.py
+++ stovell/stock_portfolio.py
@@ -1,27 +1,33 @@
 import random
 import json
+import math
 
 
 def run():
     """
     Run a sample stock portfolio script
     """
-
-    number_of_stocks = random.randint(0,100)
-    number_of_days = random.randint(0,100)
+    
+    number_of_stocks = random.randint(1, 5) # Random choice 1 through 5
+    number_of_days = random.randint(250, 300) # Random choice 250 through 300
 
     portfolio = Portfolio()
     portfolio._generate(number_of_stocks, number_of_days)
 
     for stock in portfolio.stocks:
 
-        for day in range(0, number_of_days):
-
+        print(f"\nStock {stock}-index prices ordered by date.")
+        print(json.dumps(portfolio.stock_prices(stock)))
+        
+        # Don't look at day range before 250 for an accurate picture of RS
+        for day in range(250, number_of_days):
+        
             rs = portfolio.relative_strength(stock, day)
             qi = portfolio.quantile_index(stock, day)
 
-            print(f"Stock {stock} on day {day} has relative strength = {rs}, quantile {qi}")
-
+            print(
+                f"Stock {stock} on day {day} has relative strength = {rs:.2f}, quantile {qi}"
+            )
 
 
 class Stock(object):
@@ -35,34 +41,56 @@ class Stock(object):
 
 
 class Portfolio(object):
-
     def __init__(self):
         self.stocks = {}
 
     def _generate(self, number_of_stocks, number_of_days):
+        """Randomly generates a Portfolio of Stock objects given a number of 
+        stocks and number of days.
+        """
         for stock_index in range(0, number_of_stocks):
             self.stocks[stock_index] = [
-            Stock(i, stock_index, random.randint(1,100)) 
-            for i in range(0,number_of_days)
+                Stock(i, stock_index, random.randint(1, 100))
+                for i in range(0, number_of_days)
             ]
 
+    def stock_listing(self, stock_index):
+        """Returns a list of Stock objects given a stock index
+        """
+        return self.stocks[stock_index]
+
+    def stock_prices(self, stock_index):
+        """Returns a list of Stock prices given a stock index, assume 
+        pre-sorted by date index (order)
+        """
+        stock_listing = self.stock_listing(stock_index)
+        return [stock.price for stock in stock_listing]
+
     def relative_strength(self, stock_index, day_index):
-        stock_listing = self.stocks[stock_index]
-        day_of_price = stock_listing[day_index-1].price
+        """Returns a float as a result of relative strength formula
+        """
+        stock_listing = self.stock_listing(stock_index)
+        day_of_price = stock_listing[day_index - 1].price
 
-        stock_prices = [stock.price for stock in stock_listing]
+        stock_prices = self.stock_prices(stock_index)
 
-        price_list_last_250_days = [0] + stock_prices[max(day_index-250,0):day_index]
+        # Start with stock price 0 to ensure list length of at least 2 days 
+        # are used in min/max comparison
+        price_list_last_250_days = [0] + stock_prices[
+            max(day_index - 250, 0) : day_index
+        ]
 
         min_price_last_250_days = min(price_list_last_250_days)
         max_price_last_250_days = max(price_list_last_250_days)
 
-        return (day_of_price - min_price_last_250_days) / (max_price_last_250_days - min_price_last_250_days + 1 * 10**-6)
+        return (day_of_price - min_price_last_250_days) / (
+            max_price_last_250_days - min_price_last_250_days + 1 * 10 ** -6
+        )
 
     def quantile_index(self, stock_index, day_index):
-        stock_listing = self.stocks[stock_index]
-        stock_prices = [stock.price for stock in stock_listing]
-
+        """Returns a float as a result of relative quantile index
+        """
+        stock_prices = self.stock_prices(stock_index)
         day_of_relative_strength = self.relative_strength(stock_index, day_index)
 
         strengths = []
@@ -74,10 +102,8 @@ class Portfolio(object):
 
         for index, strength in enumerate(sorted_strengths):
             if strength >= day_of_relative_strength:
-                return round(10*index/len(sorted_strengths))
+                return math.floor(10 * index / len(sorted_strengths))
 
 
 if __name__ == "__main__":
     run()
-
-
